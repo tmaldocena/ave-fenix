cdfafd6b51ac4896ce8a664821c6fd5bdbef7027
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2025-06-22 19:15:07 -0300

    first upload

---
+++ src/scripts/glightbox.js
@@ -0,0 +1,8 @@
+import GLightbox from 'glightbox';
+import 'glightbox/dist/css/glightbox.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  GLightbox({
+    selector: '.glightbox',
+  });
+});
