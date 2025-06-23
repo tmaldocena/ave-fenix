3d71ef16c39b86f86fdc5419896ddca5fcde42d5
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2025-06-22 21:13:02 -0300

    fix: gallery wont trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({  
-      vite: {
-          plugins: [tailwindcss()],  
-      },
-      adapter: vercel(),
-    });
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    output: 'server',
+    adapter: vercel({
+        edgeMiddleware: true,
+    }),
+});
